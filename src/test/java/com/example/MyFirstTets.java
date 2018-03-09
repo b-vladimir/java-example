b08e6789105ee0a34e0d6428ccb4182ff11a4bb8
b-vladimir <37156007+b-vladimir@users.noreply.github.com>
2018-03-09 16:48:17 +0200

    Initial commit

---
+++ src/test/java/com/example/MyFirstTets.java
@@ -0,0 +1,42 @@
+package com.example;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.awt.*;
+import java.util.concurrent.TimeUnit;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
+public class MyFirstTets {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public  void start(){
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+        wait = new WebDriverWait(driver, 50);
+    }
+
+    @Test
+    public void myFirstTest(){
+        driver.get("https://www.google.com/");
+        driver.findElement(By.name("q")).sendKeys("webdriver");
+        driver.findElement(By.xpath("//html//span[1]/span[1]/input[1]")).click();
+        wait.until(titleIs("webdriver - Пошук Google"));
+    }
+
+    @After
+    public void stop(){
+        driver.quit();
+        driver = null;
+    }
+}
