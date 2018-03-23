3214fafe383ba1cf165835b9c39dd965f697c592
b-vladimir <37156007+b-vladimir@users.noreply.github.com>
2018-03-23 17:20:08 +0200

    попытка взаимодействия элементов через findelements и массив

--- src/test/java/com/example/Task7.java
+++ src/test/java/com/example/Task7.java
@@ -1,8 +1,13 @@
 package com.example;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class Task7 extends TestBase {
@@ -13,12 +18,19 @@ public class Task7 extends TestBase {
         driver.findElement(By.name("username")).sendKeys("admin");
         driver.findElement(By.name("password")).sendKeys("5815");
         driver.findElement(By.name("login")).click();
+        List<WebElement> myList = driver.findElements(By.xpath("//span[@class='name']"));
+
+        for (int i =0; i<myList.size(); i++){
+            //wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("span[class=name]")));
+            myList.get(i).click();
+            Assert.assertEquals(false, driver.getTitle().isEmpty());
+        }
+
         try{
             Thread.sleep(10000);
         }catch (InterruptedException e){
             e.printStackTrace();
         }
     }
-
 
 }
--- src/test/java/com/example/TestBase.java
+++ src/test/java/com/example/TestBase.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.awt.*;
 import java.util.concurrent.TimeUnit;
