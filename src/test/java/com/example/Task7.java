eeb6a70749b35a3c569e20654887c997a08b1e1a
b-vladimir <37156007+b-vladimir@users.noreply.github.com>
2018-03-24 00:14:32 +0200

    Выполнение задания (не все пункты меню). Доделать с 2мя мониторами

--- src/test/java/com/example/Task7.java
+++ src/test/java/com/example/Task7.java
@@ -18,19 +18,61 @@ public class Task7 extends TestBase {
         driver.findElement(By.name("username")).sendKeys("admin");
         driver.findElement(By.name("password")).sendKeys("5815");
         driver.findElement(By.name("login")).click();
-        List<WebElement> myList = driver.findElements(By.xpath("//span[@class='name']"));
+        driver.findElement(By.xpath("//span[@class='name'][contains(text(),'Appearence')]")).click();
+        verify();
+        driver.findElement(By.cssSelector("li[id=doc-template]")).click();
+        verify();
+        driver.findElement(By.cssSelector("li[id=doc-logotype]")).click();
+        verify();
 
-        for (int i =0; i<myList.size(); i++){
-            //wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("span[class=name]")));
-            myList.get(i).click();
-            Assert.assertEquals(false, driver.getTitle().isEmpty());
-        }
+        driver.findElement(By.xpath("//span[@class='name'][contains(text(),'Catalog')]")).click();
+        verify();
+        driver.findElement(By.cssSelector("li[id=doc-catalog]")).click();
+        verify();
+        driver.findElement(By.cssSelector("li[id=doc-product_groups]")).click();
+        verify();
+        driver.findElement(By.cssSelector("li[id=doc-option_groups]")).click();
+        verify();
+        driver.findElement(By.cssSelector("li[id=doc-manufacturers]")).click();
+        verify();
+        driver.findElement(By.cssSelector("li[id=doc-suppliers]")).click();
+        verify();
+        driver.findElement(By.cssSelector("li[id=doc-delivery_statuses]")).click();
+        verify();
+        driver.findElement(By.cssSelector("li[id=doc-sold_out_statuses]")).click();
+        verify();
+        driver.findElement(By.cssSelector("li[id=doc-quantity_units]")).click();
+        verify();
+        driver.findElement(By.cssSelector("li[id=doc-csv]")).click();
+        verify();
+
+        driver.findElement(By.xpath("//span[@class='name'][contains(text(),'Countries')]")).click(); verify();
+
+        driver.findElement(By.xpath("//span[@class='name'][contains(text(),'Customers')]")).click(); verify();
+        driver.findElement(By.cssSelector("li[id=doc-customers]")).click();
+        verify();
+        driver.findElement(By.cssSelector("li[id=doc-csv]")).click();
+        verify();
+        driver.findElement(By.cssSelector("li[id=doc-newsletter]")).click();
+        verify();
+
+        driver.findElement(By.xpath("//span[@class='name'][contains(text(),'Geo Zones')]")).click(); verify();
+
+        driver.findElement(By.xpath("//span[@class='name'][contains(text(),'Languages')]")).click(); verify();
+        driver.findElement(By.cssSelector("li[id=doc-languages]")).click(); verify();
+        driver.findElement(By.cssSelector("li[id=doc-storage_encoding]")).click(); verify();
+
 
         try{
-            Thread.sleep(10000);
+            Thread.sleep(5000);
         }catch (InterruptedException e){
             e.printStackTrace();
         }
+    }
+
+    public void verify(){
+        Assert.assertEquals(false, driver.getTitle().isEmpty());
+        System.out.println("Title is ok");
     }
 
 }
