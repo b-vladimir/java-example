962a80c11c62d55b0def4d60bb98a69b51e2fbb7
b-vladimir <37156007+b-vladimir@users.noreply.github.com>
2018-04-27 16:42:49 +0300

    продолжаю выполнение 11 задания(разобрался как работать с выпадающим меню)

--- src/test/java/com/example/Task11.java
+++ src/test/java/com/example/Task11.java
@@ -11,9 +11,14 @@ package com.example;
 
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
 
 public class Task11 extends TestBase{
-    private String [] user = {"a", "12345", "adfhf@dfg.sdf", "1234567890", "123"};
+    private String [] user = {"a", "12345", "adfhf@dfg.sdf", "1234567890", "123", "Bolivia"};
+    private Select select;
 
     @Test
     public void test(){
@@ -25,14 +30,20 @@ public class Task11 extends TestBase{
         driver.findElement(By.cssSelector("tr > td> input[name=address1]")).sendKeys(user[0]);
         driver.findElement(By.cssSelector("tr > td> input[name=postcode]")).sendKeys(user[1]);
         driver.findElement(By.cssSelector("tr > td> input[name=city]")).sendKeys(user[0]);
-        driver.findElement(By.cssSelector("span > span.select2-selection__arrow")).click();
-        driver.findElement(By.cssSelector("li.select2-results_option[text='Bolivia']")).click();
+        WebElement element = driver.findElement(By.xpath("//*[@id=\"create-account\"]/div/form/table/tbody/tr[5]/td[1]/select"));
+        select = getSelect(element);
+        select.selectByVisibleText(user[5]);
         driver.findElement(By.cssSelector("tr > td> input[name=email]")).sendKeys(user[2]);
         driver.findElement(By.cssSelector("tr > td> input[name=phone]")).sendKeys(user[3]);
         driver.findElement(By.cssSelector("tr > td> input[name=password]")).sendKeys(user[4]);
         driver.findElement(By.cssSelector("tr > td> input[name=confirmed_password]")).sendKeys(user[4]);
         //driver.findElement(By.cssSelector("tr > td> input[name=create_account]")).click();
 
+    }
+
+    public Select getSelect(WebElement element) {
+        select = new Select(element);
+        return select;
     }
 
 }
