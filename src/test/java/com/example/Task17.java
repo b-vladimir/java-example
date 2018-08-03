67bd1bfec3146410d25a7fd6455e53a4cea7b692
b-vladimir <37156007+b-vladimir@users.noreply.github.com>
2018-08-03 20:03:28 +0300

    continue Task 17

--- src/test/java/com/example/Task17.java
+++ src/test/java/com/example/Task17.java
@@ -12,7 +12,7 @@ public class Task17 extends TestBase {
     public void test(){
         driver.navigate().to("http://localhost/litecart/admin/?app=catalog&doc=catalog&category_id=2");
         autrz();
-        List<WebElement> myList = driver.findElements(By.xpath("//tbody/tr[@class='row'][td[1]/input[starts-with(@name, 'products')]"));
+        List<WebElement> myList = driver.findElements(By.xpath("//tbody/tr[@class='row'][td[1]/input[contains(@name, 'products')]]"));
         System.out.println("Breakpoint");
     }
 }
